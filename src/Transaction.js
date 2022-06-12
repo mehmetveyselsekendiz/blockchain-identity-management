5a1971d5ea44e45929e3003bac65a5e253a9783b
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-06-12 18:53:45 +0300

    ADD: Transaction class file is created

---
+++ src/Transaction.js
@@ -0,0 +1,9 @@
+class Transaction{
+    constructor(from_address, to_address, amount){
+        this.from_address = from_address
+        this.to_address = to_address
+        this.amount = amount
+    }
+}
+
+module.exports = Transaction
