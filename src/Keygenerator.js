257a8a67ac53b1707aa5bb9db6e470efbd8df9ff
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-06-13 00:20:41 +0300

    ADD: KEygenerator is created with elliptic

---
+++ src/Keygenerator.js
@@ -0,0 +1,11 @@
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+
+const key = ec.genKeyPair();
+
+const public_key = key.getPublic('hex')
+console.log("public-key", public_key)
+const private_key = key.getPrivate('hex')
+console.log("private-key", private_key)
+
+module.exports = ec
