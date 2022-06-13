9173fa1099abbda1e41580c2765f9204b20b071c
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-06-13 03:46:58 +0300

    ADD: courses and student keys are added to generatedKey script

--- generatedKey.js
+++ generatedKey.js
@@ -1,6 +1,19 @@
-sample_key = {
-public_key : "048a0b435ee49f36ab85aa1f5f6949e7b19e09bd6c59fc69022355771fea7e52dd0a925fa521a3ac3822098edf422270ab07bd1be2ddafe80cae86391d10977ae8",
-private_key : "efd087cb88064c32d1675696c0dd19521fe437ccf016efd6a9d967e07253cf1a"
+course1_key = {
+    public_key : "04efaeee0c992b9bb487b9cfa7a9f2db0e9b028c0c74dc05a9c7ac9f851a86bdb994b921ffda0c01852666fcaf2fec9e232ce0b72778cada862159d8f21a82337f",
+    private_key : "427230ba1e21ca74db332cf562fd51d5274e39548d4c8b5e4fbbc6edb958ad41"
 }
 
-module.exports = sample_key
+
+course2_key = {
+    public_key : "04f81dbfd31117eb71dcfdfbe983778ec2c4c8d92d65bc28a0bce9363f45d4af5e6ff9a14c04d1f2b8338dd962b17b27e03b83b81ad8138d6b4f81db09aca42d67",
+    private_key : "ba6dc7d2cc83ebef3f13117f3fb695978ff13cf47822ac873f90f36ccd61c5ca"
+}
+
+student_key = {
+    public_key : "04c9d10bd99cd9321e6ad776659546738e3fe85000ab74980eca6496fa471fe2c3134d407011cc02ca1ae7b961ca2caffe370e84d66e5677b2998c900bad0d123c",
+    private_key : "f303b9f49f10665f4f3045a3e1f3dfeaf48edb4e315f8713cf9dd64dfcfd23a0"
+}
+
+
+
+module.exports = {course1_key, course2_key, student_key}
