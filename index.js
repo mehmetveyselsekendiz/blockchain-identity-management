75d1d9ca1ab19445cfaa3fb83e42fbd1641c4890
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-06-13 11:59:39 +0300

    ADD: Register student, Authorize student, Join class, Student track and Chain validation applications are done

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const Blockchain = require("./src/Blockchain")
 const Transaction = require("./src/Transaction")
 const ec = require("./src/Keygenerator")
-const {course1_key, course2_key, student_key} = require('./generatedKey')
+const {course1_key, admin_key, student_key, course2_key} = require('./generatedKey')
 
 const distance_education = new Blockchain()
+
+const ad_key = ec.keyFromPrivate(admin_key.private_key)
+const ad_wallet = ad_key.getPublic('hex')
 
 const st_key = ec.keyFromPrivate(student_key.private_key)
 const st_wallet = st_key.getPublic('hex')
 
-distance_education.giveaway_coin(st_wallet, 100)
+// Register a student
+const register_student = new Transaction(ad_wallet, null, 0, [], st_wallet)
+register_student.sign_transaction(ad_key)
+distance_education.add_operation(register_student)
+distance_education.mine_pending_tansactions("miner")
+console.log('register', distance_education.get_registration(st_wallet))
 
-const join_course1 = new Transaction(st_wallet, course1_key.public_key, 25)
+// Authorize the student for Course 1
+const authorize_student = new Transaction(ad_wallet, st_wallet, 0, [course1_key.public_key], "")
+authorize_student.sign_transaction(ad_key)
+distance_education.add_operation(authorize_student)
+distance_education.mine_pending_tansactions("miner")
+console.log('authorization', distance_education.get_course_authority(st_wallet))
+
+// Student joins Course 1
+const join_course1 = new Transaction(st_wallet, course1_key.public_key, 0, [], "")
 join_course1.sign_transaction(st_key)
-distance_education.add_transaction(join_course1)
-
-const join_course2 = new Transaction(st_wallet, course2_key.public_key, 35)
-join_course2.sign_transaction(st_key)
-distance_education.add_transaction(join_course2)
-
-console.log("miner starts...")
+distance_education.add_operation(join_course1)
 distance_education.mine_pending_tansactions("miner")
 
-console.log("student1 balance", distance_education.get_address_balance(st_wallet))
+// Student tries to join Course 2 --- Throws error because the student has no authorization for Course 2
+// const join_course2 = new Transaction(st_wallet, course2_key.public_key, 0, [], "")
+// join_course2.sign_transaction(st_key)
+// distance_education.add_operation(join_course2)
+// distance_education.mine_pending_tansactions("miner")
+
 console.log("student1 track", distance_education.get_transactions_for_wallet(st_wallet))
 console.log("Is blockchain is valid? " + distance_education.is_chain_valid())
