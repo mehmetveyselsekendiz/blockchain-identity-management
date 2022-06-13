7358cbcb145c894dff318c217b763513f3c71e4c
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-06-13 04:47:00 +0300

    STYLE: log commented

--- src/Keygenerator.js
+++ src/Keygenerator.js
@@ -4,8 +4,8 @@ const ec = new EC('secp256k1')
 const key = ec.genKeyPair();
 
 const public_key = key.getPublic('hex')
-console.log("public-key", public_key)
+//console.log("public-key", public_key)
 const private_key = key.getPrivate('hex')
-console.log("private-key", private_key)
+//console.log("private-key", private_key)
 
 module.exports = ec
