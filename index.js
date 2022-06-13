49046111a2ad2b6291bfb1f00c808ee0c03016a5
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-06-13 04:52:06 +0300

    ADD: basic distance education is modelled

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// ref: https://www.youtube.com/watch?v=zVqczFZr124
 const Blockchain = require("./src/Blockchain")
 const Transaction = require("./src/Transaction")
+const ec = require("./src/Keygenerator")
+const {course1_key, course2_key, student_key} = require('./generatedKey')
 
-const mvs_coin = new Blockchain()
-mvs_coin.create_transaction(new Transaction("user1", "user2", 100))
-mvs_coin.create_transaction(new Transaction("user2", "user3", 100))
+const distance_education = new Blockchain()
+
+const st_key = ec.keyFromPrivate(student_key.private_key)
+const st_wallet = st_key.getPublic('hex')
+
+distance_education.giveaway_coin(st_wallet, 100)
+
+const join_course1 = new Transaction(st_wallet, course1_key.public_key, 25)
+join_course1.sign_transaction(st_key)
+distance_education.add_transaction(join_course1)
+
+const join_course2 = new Transaction(st_wallet, course2_key.public_key, 35)
+join_course2.sign_transaction(st_key)
+distance_education.add_transaction(join_course2)
 
 console.log("miner starts...")
-mvs_coin.mine_pending_tansactions("miner")
+distance_education.mine_pending_tansactions("miner")
 
-console.log(JSON.stringify(mvs_coin, null, 4))
-console.log("Is blockchain is valid? " + mvs_coin.is_chain_valid())
+console.log("student1 balance", distance_education.get_address_balance(st_wallet))
+console.log("student1 track", distance_education.get_transactions_for_wallet(st_wallet))
+console.log("Is blockchain is valid? " + distance_education.is_chain_valid())
